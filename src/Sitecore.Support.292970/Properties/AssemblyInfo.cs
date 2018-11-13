0c2f9a348fd85b30f636cf39ea4f455b7d99996a
Philippe Fillion <pfi@sitecore.net>
2018-11-13 17:10:20 -0500

    Initial Commit

---
+++ src/Sitecore.Support.292970/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.292970")]
+[assembly: AssemblyProduct("Sitecore.Support.292970")]
+[assembly: ComVisible(false)]
